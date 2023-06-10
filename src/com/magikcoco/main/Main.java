65535256ddb5802734fc029521ccdfc33920f43f
homepc <punishedtomato@gmail.com>
2023-06-09 20:02:35 -0400

    initial commit

---
+++ src/com/magikcoco/main/Main.java
@@ -0,0 +1,12 @@
+package com.magikcoco.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        //TODO: https://katawashoujo.fandom.com/wiki/Transcripts
+        //TODO: process data: dialogue only (remove narrator) scripts, single speaker line banks (no duplicate lines), prompt/response pairs for each character
+        //TODO: Stats: how many unique lines per character, how many prompt/response pairs, etc
+        //TODO: Rin is also referred to a "Strange Girl", other notes here
+    }
+}
